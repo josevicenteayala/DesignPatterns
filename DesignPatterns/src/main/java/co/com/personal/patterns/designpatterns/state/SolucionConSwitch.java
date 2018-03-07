cec8b208ed60d6fe8ef738c9357608322d02190b
DESKTOP-4LLK40V\Asus <jayala@intergrupo.com>
2018-03-07 08:48:13 -0500

    Add SolucionConSwitch

---
+++ DesignPatterns/src/main/java/co/com/personal/patterns/designpatterns/state/SolucionConSwitch.java
@@ -0,0 +1,41 @@
+package co.com.personal.patterns.designpatterns.state;
+
+public class SolucionConSwitch {
+
+	private static final int CODIGO_ESTADO_TRANSITORIO = 2;
+	private static final int CODIGO_ESTADOINICIAL = 1;
+
+	public static void main(String[] args) {
+		String estadoSiguiente = identificacionEstado(CODIGO_ESTADOINICIAL);
+		System.out.println("El estado actual es: "+estadoSiguiente);
+		
+		estadoSiguiente = identificacionEstado(CODIGO_ESTADO_TRANSITORIO);
+		System.out.println("El estado actual es: "+estadoSiguiente);
+		
+		estadoSiguiente = identificacionEstado(9);
+		System.out.println("El estado actual es: "+estadoSiguiente);		
+	}
+	
+
+	/**
+	 * 
+	 */
+	private static String identificacionEstado(int estado) {
+		String estadoActual = "estadoInicial";
+		
+		switch(estado) {
+		case CODIGO_ESTADOINICIAL: {
+			estadoActual = "estadoTransitorio";
+			break;
+		}
+		case CODIGO_ESTADO_TRANSITORIO:{
+			estadoActual = "estadoFinal";
+			break;
+		}
+		default: estadoActual = "estadoInicial";
+		}
+		
+		return estadoActual;
+	}
+
+}
