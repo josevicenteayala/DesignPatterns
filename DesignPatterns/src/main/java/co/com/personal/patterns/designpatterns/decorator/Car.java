d89ebef819f0320663387207213a3dca1cf31016
DESKTOP-4LLK40V\Asus <jayala@intergrupo.com>
2018-05-18 04:39:12 -0500

    Add comments to Decorator pattern

--- DesignPatterns/src/main/java/co/com/personal/patterns/designpatterns/decorator/Car.java
+++ DesignPatterns/src/main/java/co/com/personal/patterns/designpatterns/decorator/Car.java
@@ -1,5 +1,13 @@
 package co.com.personal.patterns.designpatterns.decorator;
 
+/**
+ * @author Asus
+ * Decorator pattern allows to modify an object dinamically
+ * It is used when it's necesary to add functionality at run time 
+ * It is more flexible than inheritance
+ * The code is more simple, because you just need add functionality using simple clasess
+ * Rather than rewrite the code, it is possible to extend new code
+ */
 public interface Car {
 	void run();
 }
