3f3aadb3d13ced6554728bcd76f6a58978ffd34d
Jose Vicente Ayala Luna <joseaylu@suramericana.com.co>
2018-05-18 08:36:47 -0500

    add strategy comments

--- DesignPatterns/src/main/java/co/com/personal/patterns/designpatterns/strategy/MathOperation.java
+++ DesignPatterns/src/main/java/co/com/personal/patterns/designpatterns/strategy/MathOperation.java
@@ -1,5 +1,10 @@
 package co.com.personal.patterns.designpatterns.strategy;
 
+/**
+ * @author joseaylu
+ * 
+ *
+ */
 public interface MathOperation {
 
 	double operation(double number1, double number2);
